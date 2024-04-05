554b9d5751b51c6b55fa82431e0a5e7b983cf207
reyreey <reyhaneh179@yahoo.com>
2024-04-05 20:50:24 +0330

    grouping account by balance

---
+++ src/main/java/org/example/AccountGroup.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.example.DTO.AccountDTO;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class AccountGroup {
+    public static void groupByBalance(List<AccountDTO> accountList){
+
+        Map<String,List<AccountDTO>> accountByBalance=accountList
+                .stream()
+                .collect(Collectors.groupingBy(a -> {
+                    if (a.getAccountBalance()<=10000000) return "Group C";
+                    else if (a.getAccountBalance()<=500000000) return "Group B";
+                    return "Group A";
+                }));
+
+        accountByBalance
+                .forEach((group,a)-> System.out.format("Account %s: %s\n",group,a
+                        .stream()
+                        .map(AccountDTO::getAccountNumber)
+                        .toList()
+                        )
+                );
+    }
+}

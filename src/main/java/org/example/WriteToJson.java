49b77a00088463f2514ecef88b2fbcc546985b05
reyreey <reyhaneh179@yahoo.com>
2024-04-05 20:51:02 +0330

    serialization

---
+++ src/main/java/org/example/WriteToJson.java
@@ -0,0 +1,53 @@
+package org.example;
+
+/**
+ * @author : Reyreey
+ * @mailto : dehghan.reyhaneh179@gmail.com
+ * @created : 3/31/2024, Sunday
+ **/
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.DTO.AccountDTO;
+import org.example.DTO.CustomerDTO;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class WriteToJson {
+    /**
+     * serialize customer objects to json file
+     * @param customerList
+     */
+    public static void WriteCustomersTOJson(List<CustomerDTO> customerList){
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            // Java objects to JSON file
+            mapper.writeValue(new File("src/main/resources/outputFiles/JSONFiles/customer.json"), customerList);
+
+            // Java objects to JSON string - pretty-print
+            String jsonInString2 = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(customerList);
+            System.out.println(jsonInString2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * serialize account objects to json file
+     * @param accountList
+     */
+    public static void WriteAccountTOJson(List<AccountDTO> accountList){
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            // Java objects to JSON file
+            mapper.writeValue(new File("src/main/resources/outputFiles/JSONFiles/account.json"), accountList);
+
+            // Java objects to JSON string - pretty-print
+            String jsonInString2 = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(accountList);
+            System.out.println(jsonInString2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/WriteToXml.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import org.example.DTO.AccountDTO;
+import org.example.DTO.CustomerDTO;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author : Reyreey
+ * @mailto : dehghan.reyhaneh179@gmail.com
+ * @created : 3/31/2024, Sunday
+ **/
+public class WriteToXml {
+    public static void WriteCustomersToXml(List<CustomerDTO> customerList) throws IOException {
+        XmlMapper xmlMap = new XmlMapper();
+        xmlMap.writeValue(new File("src/main/resources/outputFiles/XMLFiles/customer.xml"), customerList);
+    }
+
+    public static void WriteAccountsToXml(List<AccountDTO> accountList) throws IOException {
+        XmlMapper xmlMap = new XmlMapper();
+        xmlMap.writeValue(new File("src/main/resources/outputFiles/XMLFiles/account.xml"), accountList);
+    }
+}
